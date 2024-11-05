83af17a06d45b052ea5261e94753b6250bad03c9
Lopeseric09 <151591108+Lopeseric09@users.noreply.github.com>
2024-11-05 04:58:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
             name: name,
             quantity: quantity,
             price: price,
-            category: Número de série,
+            category: category,
             expiry: expiry,
             location: location,
         };
