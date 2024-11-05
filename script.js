216adf5e3521ba757df872695c8800498ea37fbe
Lopeseric09 <151591108+Lopeseric09@users.noreply.github.com>
2024-11-05 03:16:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', () => {
         addQuantityButtons.forEach(button => {
             button.addEventListener('click', (e) => {
                 const index = e.target.dataset.index;
-                changeQuantity(index, 0.1); // Aumenta a quantidade em 0.1
+                changeQuantity(index, 1); // Aumenta a quantidade em 1
             });
         });
 
@@ -94,7 +94,7 @@ document.addEventListener('DOMContentLoaded', () => {
         removeQuantityButtons.forEach(button => {
             button.addEventListener('click', (e) => {
                 const index = e.target.dataset.index;
-                changeQuantity(index, -0.1); // Diminui a quantidade em 0.1
+                changeQuantity(index, -1); // Diminui a quantidade em 1
             });
         });
     }
